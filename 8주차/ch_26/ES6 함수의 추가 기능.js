5a9e420dbd0ebb18e9c9ad0c873c8ae28162c9b7
kimgyejeong <pmwkdo@gmail.com>
2024-11-10 01:32:44 +0900

    deep dive ch26, 27

---
+++ 8주차/ch_26/ES6 함수의 추가 기능.js
@@ -0,0 +1,47 @@
+console.log('helloworld');
+// ch.26 ES6 함수의 추가 기능
+
+// 1. 기본 매개변수
+function calculatePrice(price, tax = 0.1) {
+    return price + (price * tax);
+}
+
+console.log(calculatePrice(100));
+console.log(calculatePrice(100, 0.2));
+console.log(calculatePrice(100, 0.5, 'kim'));
+
+/**
+ *      110
+ *      120
+ *      150
+ */
+
+// 2. 나머지 매개변수
+function sumAll(...numbers) {
+    return numbers.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(sumAll(1, 2, 3, 4));    // 10
+
+// 3. 화살표 함수
+const person = {
+    name: 'John',
+    sayHi: () => `Hi, I'm ${this.name}`
+};
+
+console.log(person.sayHi());
+// Hi, I'm undefined
+// 화살표 함수는 this가 함수가 정의된 문맥을 따릅니다. sayHi 메서드에서 화살표 함수를 사용했기 때문에, this는 person 객체가 아닌 전역 객체를 가리킵니다. 전역 객체에는 name 속성이 없으므로 결과는 Hi, I'm undefined가 됩니다.
+
+
+const person2 = {
+    name: 'John',
+    sayHi: function() {
+        return `Hi, I'm ${this.name}`;
+    }
+};
+
+console.log(person2.sayHi());
+// Hi, I'm John
+
+
---
+++ 8주차/ch_27/배열.js
@@ -0,0 +1,20 @@
+console.log('helloworld');
+// ch.27 배열
+
+// const numbers = [1, 2, 3];
+// let sum = 0;
+// numbers.forEach(num => sum += num);
+// console.log(sum)// 6
+
+
+// const numbers = [1, 2, 3];
+// const doubled = numbers.map(num => num * 2);
+// console.log(doubled);   // [2, 4, 6]
+
+// const numbers = [1, 2, 3, 4, 5];
+// const evenNumbers = numbers.filter(num => num % 2 === 0);
+// console.log(evenNumbers);
+
+// const numbers = [1, 2, 3, 4];
+// const result = numbers.reduce((acc, curr) => acc + curr, 0);
+// console.log(result);
