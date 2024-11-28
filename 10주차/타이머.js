e343a8a59d62205e9c59d44b4e87b209a0330328
GyeJeongKim <pmwkdo@bgntech.com>
2024-11-28 12:47:02 +0900

    deep dive ch41,42

--- 10주차/타이머.js
+++ 10주차/타이머.js
@@ -1,2 +1,11 @@
 //setTimeout 한번실행
 //setInterval 타이머가 만료될 때마다 반복 호출
+
+console.log('hello world');
+
+// setTimeout(()=>{
+//     setTimeout(()=>{
+//         console.log('inner')
+//     },500);
+//     console.log('outer;')
+// },1000);
