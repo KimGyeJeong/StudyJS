867c1fa9e3e8e9b76d661e10948b70eec7a833a2
KimGyeJeong <pmwkdo@gmail.com>
2025-01-07 22:27:10 +0900

    express 서버 미들 웨어 사용 #2

--- nodeJS/5주차/index.js
+++ nodeJS/5주차/index.js
@@ -1,29 +1,49 @@
 const express = require('express');
 const path = require("path");
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
 
-//미들웨어
-app.use((req, res, next) => {
-    console.log('모든 요청이 실행됨');
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie:{
+        httpOnly: true,
+        secure: false,
+    },
+    name:'session-cookie',
+}));
+
+app.use((req,res,next)=>{
+    console.log('모든 요청에 실행');
     next();
 })
 
-app.get('/', (req, res, next) => {
-    console.log('GET / 요청에서 실행');
+app.get('/',(req,res, next)=>{
+    console.log('GET / 요청에서만 실행');
     next();
-}, (req, res) => {
-    throw new Error('에러는 에러처리 미들웨어로 감');
+}, (req, res)=>{
+    console.log('실행!');
+    throw new Error('에러는 에러 처리 미들웨어로 감.');
 });
 
-app.use((err, req, res, next) => {
-    console.log('여기는 에러처리하는곳');
+app.use((err,req, res)=>{
     console.error(err);
     res.status(500).send(err.message);
 });
 
 app.listen(app.get('port'), () => {
     console.log(`Server started on port ${app.get('port')}`);
-})
+});
