86da8de5e7324182c4e89a59e2b56d50e4483efa
KimGyeJeong <pmwkdo@gmail.com>
2025-01-08 00:09:21 +0900

    express 서버 미들 웨어 사용 #3

--- nodeJS/5주차/index.js
+++ nodeJS/5주차/index.js
@@ -10,11 +10,23 @@ const app = express();
 
 app.set('port', process.env.PORT || 5000);
 
+// morgan. 로그용 
 app.use(morgan('dev'));
+
+// static. 정적인 파일들을 제공하는 라우터 역할.
+//public 폴더를 만들고 css나 js, 이미지 파일들을 public 폴더에 넣으면 브라우저에서 접근할 수 있음
 app.use('/', express.static(path.join(__dirname, 'public')));
+
+// body-parser. 요청의 본문에 있는 데이터를 해석해서 req.body 객체로 만들어주는 미들웨어.
+//json 형식의 데이터 전달 방식.
 app.use(express.json());
+//extended가 false 이면 querystring 모듈을 사용하여 쿼리스트링을 해석하고, true 이면 qs 모듈을 사용하여 쿼리스트링을 해석함.
 app.use(express.urlencoded({ extended: false }));
+
+// cookie-parser. 요청에 동봉된 쿠키를 해석해 req.cookies 객체로 만듬.
 app.use(cookieParser(process.env.SECRET));
+
+// express-session. 세션 관리용 미들웨어
 app.use(session({
     resave: false,
     saveUninitialized: false,
@@ -26,6 +38,7 @@ app.use(session({
     name:'session-cookie',
 }));
 
+// 미들웨어
 app.use((req,res,next)=>{
     console.log('모든 요청에 실행');
     next();
