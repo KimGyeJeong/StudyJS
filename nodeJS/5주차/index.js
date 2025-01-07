580b213dc95beda8f898334615aa7d5b038351ee
KimGyeJeong <pmwkdo@gmail.com>
2025-01-07 22:06:06 +0900

    express 서버 미들 웨어 사용 #1

--- nodeJS/5주차/index.js
+++ nodeJS/5주차/index.js
@@ -5,8 +5,23 @@ const app = express();
 
 app.set('port', process.env.PORT || 5000);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+//미들웨어
+app.use((req, res, next) => {
+    console.log('모든 요청이 실행됨');
+    next();
+})
+
+app.get('/', (req, res, next) => {
+    console.log('GET / 요청에서 실행');
+    next();
+}, (req, res) => {
+    throw new Error('에러는 에러처리 미들웨어로 감');
+});
+
+app.use((err, req, res, next) => {
+    console.log('여기는 에러처리하는곳');
+    console.error(err);
+    res.status(500).send(err.message);
 });
 
 app.listen(app.get('port'), () => {
