dfc64c20ca5ebf2dbcc9a13ae9ef5b34ff0151cd
KimGyeJeong <pmwkdo@gmail.com>
2025-01-07 21:53:39 +0900

    express 서버 사용 #2

--- nodeJS/5주차/index.js
+++ nodeJS/5주차/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require("path");
 
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(app.get('port'), () => {
