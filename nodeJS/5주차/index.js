8303cea7ba9c96159e0b6e0c3e0b213eecf26f19
KimGyeJeong <pmwkdo@gmail.com>
2025-01-09 00:10:57 +0900

    express 서버 라우터 #1

--- nodeJS/5주차/index.js
+++ nodeJS/5주차/index.js
@@ -9,83 +9,96 @@ dotenv.config();
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
+// // morgan. 로그용 
+// app.use(morgan('dev'));
+//
+// // static. 정적인 파일들을 제공하는 라우터 역할.
+// //public 폴더를 만들고 css나 js, 이미지 파일들을 public 폴더에 넣으면 브라우저에서 접근할 수 있음
+// app.use('/', express.static(path.join(__dirname, 'public')));
+//
+// // body-parser. 요청의 본문에 있는 데이터를 해석해서 req.body 객체로 만들어주는 미들웨어.
+// //json 형식의 데이터 전달 방식.
+// app.use(express.json());
+// //extended가 false 이면 querystring 모듈을 사용하여 쿼리스트링을 해석하고, true 이면 qs 모듈을 사용하여 쿼리스트링을 해석함.
+// app.use(express.urlencoded({extended: false}));
+//
+// // cookie-parser. 요청에 동봉된 쿠키를 해석해 req.cookies 객체로 만듬.
+// app.use(cookieParser(process.env.SECRET));
+//
+// // express-session. 세션 관리용 미들웨어
+// app.use(session({
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: process.env.COOKIE_SECRET,
+//     cookie: {
+//         httpOnly: true,
+//         secure: false,
+//     },
+//     name: 'session-cookie',
+// }));
+//
+// //multer 추가
+// const multer = require('multer');
+// const fs = require('fs');
+//
+// try {
+//     fs.readdirSync('uploads');
+// } catch (err) {
+//     console.error('uploads not found. create uploads folder');
+//     fs.mkdirSync('uploads');
+// }
+//
+// const upload = multer({
+//     storage: multer.diskStorage({
+//         destination: (req, file, done) => {
+//             done(null, 'uploads/');
+//         },
+//         filename(req, file, done) {
+//             const ext = path.extname(file.originalname);
+//             done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+//         }
+//     }),
+//     limits: {fileSize: 5 * 1024 * 1024},
+// });
+//
+// app.get('/upload', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'multipart.html'));
+// });
+//
+// app.post('/upload', upload.fields([{name: 'image1'}, {name: 'image2'}]),
+//     (req, res) => {
+//         console.log(req.files, req.body);
+//         res.send('ok');
+//     },
+// );
+//
+//
+// // 미들웨어
+// app.use((req, res, next) => {
+//     console.log('모든 요청에 실행');
+//     next();
+// })
+//
+// app.get('/', (req, res, next) => {
+//     console.log('GET / 요청에서만 실행');
+//     next();
+// }, (req, res) => {
+//     console.log('실행!');
+//     throw new Error('에러는 에러 처리 미들웨어로 감.');
+// });
+//
+// app.use((err, req, res) => {
+//     console.error(err);
+//     res.status(500).send(err.message);
+// });
 
-// morgan. 로그용 
-app.use(morgan('dev'));
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/user');
+app.use('/', indexRouter);
+app.use('/user', userRouter);
 
-// static. 정적인 파일들을 제공하는 라우터 역할.
-//public 폴더를 만들고 css나 js, 이미지 파일들을 public 폴더에 넣으면 브라우저에서 접근할 수 있음
-app.use('/', express.static(path.join(__dirname, 'public')));
-
-// body-parser. 요청의 본문에 있는 데이터를 해석해서 req.body 객체로 만들어주는 미들웨어.
-//json 형식의 데이터 전달 방식.
-app.use(express.json());
-//extended가 false 이면 querystring 모듈을 사용하여 쿼리스트링을 해석하고, true 이면 qs 모듈을 사용하여 쿼리스트링을 해석함.
-app.use(express.urlencoded({extended: false}));
-
-// cookie-parser. 요청에 동봉된 쿠키를 해석해 req.cookies 객체로 만듬.
-app.use(cookieParser(process.env.SECRET));
-
-// express-session. 세션 관리용 미들웨어
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false,
-    },
-    name: 'session-cookie',
-}));
-
-//multer 추가
-const multer = require('multer');
-const fs = require('fs');
-
-try {
-    fs.readdirSync('uploads');
-} catch (err) {
-    console.error('uploads not found. create uploads folder');
-    fs.mkdirSync('uploads');
-}
-
-const upload = multer({
-    storage: multer.diskStorage({
-        destination: (req, file, done) => {
-            done(null, 'uploads/');
-        },
-        filename(req, file, done) {
-            const ext = path.extname(file.originalname);
-            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
-        }
-    }),
-    limits: {fileSize: 5 * 1024 * 1024},
-});
-
-app.get('/upload', (req, res) => {
-    res.sendFile(path.join(__dirname, 'multipart.html'));
-});
-
-app.post('/upload', upload.fields([{name: 'image1'}, {name: 'image2'}]),
-    (req, res) => {
-        console.log(req.files, req.body);
-        res.send('ok');
-    },
-);
-
-
-// 미들웨어
 app.use((req, res, next) => {
-    console.log('모든 요청에 실행');
-    next();
-})
-
-app.get('/', (req, res, next) => {
-    console.log('GET / 요청에서만 실행');
-    next();
-}, (req, res) => {
-    console.log('실행!');
-    throw new Error('에러는 에러 처리 미들웨어로 감.');
+    res.status(404).send('page not found');
 });
 
 app.use((err, req, res) => {
